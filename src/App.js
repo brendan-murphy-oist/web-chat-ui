baf119b1f0cc232050df75f0b8fd691e978b3d2d
brendan-murphy-oist <88812361+brendan-murphy-oist@users.noreply.github.com>
2021-08-16 10:56:56 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MinimizableWebChat from './MinimizableWebChat';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <MinimizableWebChat />
+  </div>
+);
+
+export default App;
