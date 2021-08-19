63042f0d6e060f70cb946828877a49f34efe44bc
brendan-murphy-oist <88812361+brendan-murphy-oist@users.noreply.github.com>
2021-08-19 15:15:34 +0900

    Update MinimizableWebChat.js
    Now looks for a token at https://webchat-mockbot.azurewebsites.net/directline/token instead of localhost, this way it will still talk to a bot (not our bot, but a bot) during testing. Once our token-server is working, we will build a new version and point it at that.

--- src/MinimizableWebChat.js
+++ src/MinimizableWebChat.js
@@ -51,7 +51,7 @@ const MinimizableWebChat = () => {
     if (!token) {
       console.log('getting a token')
       //In production, this will need to be the URL where the bot 
-      const res = await fetch('http://localhost:5000/api/directline/token', { method: 'GET' });
+      const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', { method: 'GET' });
       console.log(res)
       const { token } = await res.json();
       console.log(token)
